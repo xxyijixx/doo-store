bf79b8ff3e45a1c9ccdeb9e8addb07e58104a0c2
yijixx <xxyijixx@gmail.com>
2024-10-30 15:12:39 +0800

    perf: 数据库字段长度

--- backend/core/model/app_detail.go
+++ backend/core/model/app_detail.go
@@ -3,10 +3,10 @@ package model
 type AppDetail struct {
 	BaseModel
 	AppID         int64  `json:"appId"`
-	Version       string `json:"version" gorm:"size:60;not null;default:''"`
-	Params        string `json:"-"`
-	DockerCompose string `json:"dockerCompose"`
-	Status        string `json:"status" gorm:"size:60;not null;default:''"`
+	Version       string `json:"version" gorm:"size:40;not null;default:''"`
+	Params        string `json:"-" gorm:"type:text"`
+	DockerCompose string `json:"dockerCompose" gorm:"type:text"`
+	Status        string `json:"status" gorm:"size:200;not null;default:''"`
 }
 
 func (*AppDetail) TableName() string {
