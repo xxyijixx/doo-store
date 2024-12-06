7bbd33f62d1630b83fbe34e9ca21570c73c5c655
nuohh <930699428@qq.com>
2024-12-06 13:00:42 +0800

    tip：分页优化

--- web/src/components/tabsnav/paginationcom.tsx
+++ web/src/components/tabsnav/paginationcom.tsx
@@ -8,6 +8,7 @@ import {
     // PaginationEllipsis,
 } from "@/components/ui/pagination"
 import { useState } from "react"
+import { Input } from "@/components/ui/input";
 
 interface PaginationComProps {
     currentPage: number;
@@ -42,7 +43,7 @@ export function PaginationCom({
     };
 
     return (
-        <div className="flex items-center justify-end gap-4 text-sm mr-6 lg:mt-6 md:mt-6 mt-6">
+        <div className="flex items-center justify-end lg:gap-4 md:gap-4 text-sm mr-6 lg:mt-6 md:mt-6 mt-6">
             <span>共 {totalItems || 0} 条</span>
             
             <div className="flex items-center list-none">
@@ -86,17 +87,17 @@ export function PaginationCom({
                 {pageSize}条/页
             </div>
             
-            <div className="items-center gap-2 lg:flex md:flex hidden">
+            <div className="items-center gap-2 lg:flex md:flex flex">
                 <span>跳至</span>
-                <input
-                    type="number"
+                <Input
+                    // type="number"
                     min={1}
                     max={totalPages}
                     value={jumpPage}
                     onChange={(e) => setJumpPage(e.target.value)}
                     onKeyPress={handleKeyPress}
                     onBlur={handleJump}
-                    className="border rounded w-12 px-2 mx-0.5 py-1"
+                    className="border  active:border-gray-200 rounded w-9 px-2 mx-0.5 py-1"
                 />
                 <span>页</span>
             </div>
